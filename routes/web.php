6f89804b5927d91b03a8ebaef9ac987170411a83
Fitri HY <155282015+fitri-hy@users.noreply.github.com>
2024-11-29 13:19:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@ use Core\Router;
 
 use App\Controllers\HomeController;
 use App\Controllers\AboutController;
-use App\Controllers\ErrorController;
 
 Router::get('/', [HomeController::class, 'index']);
 Router::get('/about', [AboutController::class, 'index']);
